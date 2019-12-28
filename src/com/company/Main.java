8f43e4c031d3eac3b17773d915689014c071905d
Anofiles <59262319+Anofiles@users.noreply.github.com>
2019-12-28 16:10:08 +0000

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int []x=new int[5];
+        int []x=new int[50];
         for (int i = 0; i < x.length; i++) {
             x[i]= (int) (Math.random()*9);
             System.out.print(x[i]+" ");
             
         }
-        
+        int y;
+        for (int i = 0; i <x.length; i++) {
+            for (int j =0; j <x.length-1; j++) {
+                if (x[j]>x[j+1]){
+                    y=x[j];
+                    x[j]=x[j+1];
+                    x[j+1]=y;
+
+                }
+
+
+            }
+        }
+        System.out.println();
+        for (int q:x
+             ) {
+            System.out.print(q+ " ");
+
+        }
         
 
     }
